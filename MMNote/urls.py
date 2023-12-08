335a35db7341024eec24d9c2aa76d4383776140b
ResplendentGrave <1316792949@qq.com>
2023-12-08 21:32:47 +0900

    feat:gitignore init

--- MMNote/urls.py
+++ MMNote/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-
+# regist your apps in this file like 
+# path('', include('[appname].urls'))
 urlpatterns = [
     path('', include('userlogapp.urls')),
+    path('', include('mnnvoice.urls')),
 ]
