a9b48a8ff4e75af73c6e8c00befe35afe067e3f9
obarakkyo <obarakkyo@icloud.com>
2023-12-13 14:58:25 +0900

    urls setting

--- userlogapp/urls.py
+++ userlogapp/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .views import   signupfunc, loginfunc
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    # path('home/', include('mnnvoice.urls'), name='home'),
 ]
--- userlogapp/views.py
+++ userlogapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError 
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
-# Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_again = request.POST['passwordagain']
+
+        if password != password_again:
+            return render(request, 'signup.html', {'errormassage':'再入力パスワードが違います！'})
+        print(password_again)
+        try:
+            user = User.objects.create_user(username, '', password)
+            return redirect('login')
+        except IntegrityError:
+            return render(request, 'signup.html', {'errormassage':'そのusernameは既に存在しています。'})
+    return render(request, 'signup.html', {})
+
+
+
+def loginfunc(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            mnnvoice_url = reverse('mnnvoice_app:mnnvoice_home')
+            return redirect(mnnvoice_url)
+        else:
+            return render(request, 'login.html', {'errormassage':'ログインに失敗しました。再度ログインしてください。'})
+    return render(request, 'login.html', {})
