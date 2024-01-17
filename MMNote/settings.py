b11a17cbd6f526572603ed3b2355ab80ec83bc4e
ResplendentGrave <1316792949@qq.com>
2024-01-17 14:53:23 +0900

    feat: db init

--- MMNote/settings.py
+++ MMNote/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#缓存
+CACHES={
+    'default':{
+        'BACKEND':'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
