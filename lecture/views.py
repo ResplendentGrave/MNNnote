eb820191948e038ed7ada5bfa8b592cd9075ae58
obarakkyo <obarakkyo@icloud.com>
2024-01-30 21:03:07 +0900

    部分検索機能　追加

--- lecture/views.py
+++ lecture/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import LectureMpdel
 
 def lecture(request):
+    if request.POST['searchValue'] == "":
+        return render(request, 'lecture.html')
     if request.method=="POST":
-        # print('POSTリクエストが来ました！')
-        # print(request.POST)
-        models = LectureMpdel.objects.filter(name=request.POST['searchValue'])
+        models = LectureMpdel.objects.filter(name__contains=request.POST['searchValue'])
         return render(request, "lecture.html", context={'models':models})
     return render(request,"lecture.html")
